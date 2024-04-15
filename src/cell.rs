ff7174b0b6fde11d1debe69058e537f75dfc6fcb
Franco Cuppari <fcuppari@fi.uba,ar>
2024-04-15 11:31:56 -0300

    Add Cell implementation

--- src/cell.rs
+++ src/cell.rs
@@ -1,18 +1,52 @@
-#[derive(Debug, Clone)]
+/// Struct representing a cell,
+/// with its state
+#[derive(Clone)]
 pub struct Cell {
     alive: bool,
 }
 
 impl Cell {
-    pub fn new(initial_state: bool) -> Cell {
+    /// Constructor
+    pub const fn new(initial_state: bool) -> Self {
         Self {
             alive: initial_state,
         }
     }
-    pub fn is_alive(&self) -> bool {
+    /// Getter for the state
+    pub const fn is_alive(&self) -> bool {
         self.alive
     }
+    /// Setter for the state
     pub fn change_liveliness(&mut self, new_value: bool) {
         self.alive = new_value;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_cell() {
+        let cell = Cell::new(true);
+        assert_eq!(cell.is_alive(), true);
+    }
+
+    #[test]
+    fn test_default_dead_cell() {
+        let cell = Cell::new(false);
+        assert_eq!(cell.is_alive(), false);
+    }
+
+    #[test]
+    fn test_change_liveliness() {
+        let mut cell = Cell::new(true);
+        assert_eq!(cell.is_alive(), true);
+
+        cell.change_liveliness(false);
+        assert_eq!(cell.is_alive(), false);
+
+        cell.change_liveliness(true);
+        assert_eq!(cell.is_alive(), true);
     }
 }
